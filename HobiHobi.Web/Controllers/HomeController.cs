63416c9e326b56f07d69b67ba3bb5ab11c84d50b
Dody Gunawinata <dodyg@silverkeytech.com>
2012-08-18 17:50:44 +0200

    This is the first working version of the river stream formatter.

--- HobiHobi.Web/Controllers/HomeController.cs
+++ HobiHobi.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using HobiHobi.Core.Feeds;
 
@@ -11,14 +12,37 @@ namespace HobiHobi.Web.Controllers
     {
         public ActionResult Index()
         {
-            var fetch = new Fetcher();
             var hostTarget = "http://static.scripting.com";
             var pathTarget = "/houston/rivers/apple/River3.js";
 
-            var output = fetch.Download(hostTarget, pathTarget);
+            ViewBag.FeedUrl = hostTarget + pathTarget;
 
-            var river = fetch.Serialize(output);
-            return View(river);
+            var cache = this.HttpContext.Cache[ViewBag.FeedUrl];
+
+            if (cache == null)
+            {
+                var fetch = new Fetcher();
+                try
+                {
+
+                    var output = fetch.Download(hostTarget, pathTarget);
+                    var river = fetch.Serialize(output);
+
+                    HttpContext.Cache.Add(ViewBag.FeedUrl, river, null, Cache.NoAbsoluteExpiration, new TimeSpan(0, 10, 0), CacheItemPriority.Default, null);
+
+                    ViewBag.CacheStatus = "No Cache";
+                    return View(river);
+                }
+                catch (Exception ex)
+                {
+                    return Content("Sorry, we really try to process " + ViewBag.FeedUrl + " but fate has decided on something else. " + ex.Message);
+                }
+            }
+            else
+            {
+                ViewBag.CacheStatus = "Cached for 10 minutes";
+                return View(cache);
+            }
         }
     }
 }
