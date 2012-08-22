eb089d66edb07501b5e80c1242a7926b69804704
Dody Gunawinata <dodyg@silverkeytech.com>
2012-08-22 14:41:26 +0200

    Complete the parsing of the opml head.

--- HobiHobi.Core/Subscriptions/Opml.cs
+++ HobiHobi.Core/Subscriptions/Opml.cs
@@ -9,8 +9,8 @@ namespace HobiHobi.Core.Subscriptions
     public class Opml
     {
         public string Title { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
         public string OwnerName { get; set; }
         public string OwnerEmail { get; set; }
         public Uri OwnerId { get; set; }
@@ -31,17 +31,42 @@ namespace HobiHobi.Core.Subscriptions
         public void LoadFromXML(string xml)
         {
             var elements = XElement.Parse(xml);
-            var head = elements.Element("head").Descendants();
+            var heads = elements.Element("head").Descendants();
 
-            Func<string, IEnumerable<XElement>, string> select = (filter, heads) =>
+            Func<string, string> selectString = (filter) =>
                 {
                     return heads.Where(x => x.Name == filter).Select(x => x.Value).FirstOrDefault();
                 };
 
-            Title = select("title", head);
-            OwnerName = select("ownerName", head);
-            OwnerEmail = select("ownerEmail", head);
-            ExpansionState = select("expansionState", head);
+            Func<string, int?> selectInt = (filter) =>
+            {
+                return heads.Where(x => x.Name == filter).Select(x => Convert.ToInt32(x.Value)).FirstOrDefault();
+            };
+
+            Func<string, DateTime?> selectDate = (filter) =>
+            {
+                return heads.Where(x => x.Name == filter).Select(x => Convert.ToDateTime(x.Value)).FirstOrDefault();
+            };
+
+            Func<string, Uri> selectUri = (filter) =>
+            {
+                return heads.Where(x => x.Name == filter).Select(x => new Uri(x.Value)).FirstOrDefault();
+            };
+
+
+            Title = selectString("title");
+            DateCreated = selectDate("dateCreated");
+            DateModified = selectDate("dateModified");
+            OwnerName = selectString("ownerName");
+            OwnerEmail = selectString("ownerEmail");
+            OwnerId = selectUri("ownerId");
+            Docs = selectUri("docs");
+            ExpansionState = selectString("expansionState");
+            VertScrollState = selectInt("vertScrollState");
+            WindowTop = selectInt("windowTop");
+            WindowLeft = selectInt("windowLeft");
+            WindowBottom = selectInt("windowBottom");
+            WindowRight = selectInt("windowRight");
         }
     }
 }
