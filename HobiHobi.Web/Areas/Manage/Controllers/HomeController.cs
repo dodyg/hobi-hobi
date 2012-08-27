4a701d53e78c68511bbd7c68ce8571bccdd69c74
Dody Gunawinata <dodyg@silverkeytech.com>
2012-08-27 21:45:10 +0200

    Put /manage under authorization

--- HobiHobi.Web/Areas/Manage/Controllers/HomeController.cs
+++ HobiHobi.Web/Areas/Manage/Controllers/HomeController.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HobiHobi.Core.Identity;
 
 namespace HobiHobi.Web.Areas.Manage.Controllers
 {
+    [Authorize(Roles = "Participant,Admin")]
     public class HomeController : RavenController
     {
         public ActionResult Index()
