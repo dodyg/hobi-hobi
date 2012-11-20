f01e78195b16975ec178acde57d9edf42c7159a1
Dody Gunawinata <dodyg@silverkeytech.com>
2012-11-20 13:20:52 +0200

    Try another approach to check connection protocol security

--- HobiHobi.Web/Areas/Manage/Controllers/BlogController.cs
+++ HobiHobi.Web/Areas/Manage/Controllers/BlogController.cs
@@ -30,7 +30,7 @@ namespace HobiHobi.Web.Areas.Manage.Controllers
             ViewBag.BlogTitle = blog.Title;
             ViewBag.Feeds = feeds;
             ViewBag.BlogLink = blog.HtmlLink;
-            ViewBag.IsSecureConnection = Request.Url.AbsolutePath.Contains("https://");
+            ViewBag.IsSecureConnection = Request.Headers["X-Forwarded-Proto"] == "https"; //specific hack for appharbor to detect whether request is secure
             ViewBag.SecureUrl = Texts.ConvertToSecureVersion(Request.Url);
             return View();
         }
