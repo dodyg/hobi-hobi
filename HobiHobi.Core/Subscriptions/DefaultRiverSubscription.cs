72a417a06992e9e2686fc203f834cf01d638db85
Dody Gunawinata <dodyg@silverkeytech.com>
2012-11-25 10:31:07 +0200

    Add more channels for the default rivers

--- HobiHobi.Core/Subscriptions/DefaultRiverSubscription.cs
+++ HobiHobi.Core/Subscriptions/DefaultRiverSubscription.cs
@@ -18,35 +18,35 @@ namespace HobiHobi.Core.Subscriptions
 
             river.Items.Add(new RiverSubscriptionItem
             {
-                Text = "Apple River",
+                Text = "Apple",
                 Name = "apple",
                 JSONPUri = new Uri("http://static.scripting.com/houston/rivers/apple/River3.js")
             });
 
             river.Items.Add(new RiverSubscriptionItem
             {
-                Text = "Dave's River",
+                Text = "Dave",
                 Name = "dave",
                 JSONPUri = new Uri("http://static.scripting.com/houston/rivers/iowaRiver3.js")
             });
 
             river.Items.Add(new RiverSubscriptionItem
             {
-                Text = "Tech River (TechMeme)",
+                Text = "Tech Meme",
                 Name = "tech",
                 JSONPUri = new Uri("http://static.scripting.com/houston/rivers/techmeme/River3.js")
             });
 
             river.Items.Add(new RiverSubscriptionItem
             {
-                Text = "NYT River",
+                Text = "NYT",
                 Name = "nyt",
                 JSONPUri = new Uri("http://static.scripting.com/houston/rivers/nyt/River3.js")
             });
 
             river.Items.Add(new RiverSubscriptionItem
             {
-                Text = "No Agenda River",
+                Text = "No Agenda",
                 Name = "noagenda",
                 JSONPUri = new Uri("http://s3.amazonaws.com/river.curry.com/rivers/radio2/River3.js")
             });
@@ -56,6 +56,41 @@ namespace HobiHobi.Core.Subscriptions
                 Text = "East Village News",
                 Name = "eastVillageNews",
                 JSONPUri = new Uri("http://static.scripting.com/houston/rivers/eastVillageRiver3.js")
+            });
+
+            river.Items.Add(new RiverSubscriptionItem
+            {
+                Text = "Programming",
+                Name = "programmingNews",
+                JSONPUri = new Uri("http://hobieu.apphb.com/s/riverjs/programming")
+            });
+
+            river.Items.Add(new RiverSubscriptionItem
+            {
+                Text = "World",
+                Name = "worldRiver",
+                JSONPUri = new Uri("http://static.scripting.com/houston/rivers/international/River3.js")
+            });
+
+            river.Items.Add(new RiverSubscriptionItem
+            {
+                Text = "Movies",
+                Name = "moviesRiver",
+                JSONPUri = new Uri("http://static.scripting.com/houston/rivers/movies/River3.js")
+            });
+
+            river.Items.Add(new RiverSubscriptionItem
+            {
+                Text = "Podcasts",
+                Name = "podcastRiver",
+                JSONPUri = new Uri("http://static.scripting.com/houston/rivers/podcasts/River3.js")
+            });
+
+            river.Items.Add(new RiverSubscriptionItem
+            {
+                Text = "Nomadlife",
+                Name = "nomadlife",
+                JSONPUri = new Uri("http://hobieu.apphb.com/s/riverjs/nomadlife")
             });
 
             return river;
