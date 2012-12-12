79db90386fc83bc65464f22401c10a2c1bae4e55
Dody Gunawinata <dodyg@silverkeytech.com>
2012-12-12 10:41:30 +0200

    Add the opml end point and data.

---
+++ HobiHobi.Web/Areas/API/Controllers/OpmlController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HobiHobi.Web.Areas.API.Controllers
+{
+    public class OpmlController : Controller
+    {
+        //
+        // GET: /API/Opml/
+
+        public ActionResult Root()
+        {
+            var location = Server.MapPath("/content/opml/root.opml");
+            var result = new FilePathResult(location, "application/xml");
+            return result;
+        }
+    }
+}
