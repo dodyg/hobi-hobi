7180c34201d0703b675808c7fbccdca0e3b61ca6
ahmedsk <ahmedk@silverkeytech.com>
2012-12-20 15:48:22 +0200

    fixed bug

--- HobiHobi.Web/Controllers/OpmlController.cs
+++ HobiHobi.Web/Controllers/OpmlController.cs
@@ -83,8 +83,8 @@ namespace HobiHobi.Web.Controllers
             var doc = this.RavenSession.Load<EditorDocument>(id);
             if (doc == null)
             {
-                doc.Id = id;
                 doc = outlines;
+                doc.Id = id;
                 doc.DateCreated = DateTime.UtcNow.ToString("R");
                 doc.DateModified = DateTime.UtcNow.ToString("R");
             }
